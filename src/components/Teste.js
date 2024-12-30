ba6189cf3c390e99b56237a7842338275f53ab48
Ana Filipa <98351447+filipatrabucho@users.noreply.github.com>
2024-12-30 11:45:26 +0000

    Update Teste.js

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -3,33 +3,6 @@ import React from 'react'
 function Teste() {
   return (
     <>
-      <div>
-          Teste a funcionar o fork.
-      </div>
-      <p>
-         Paragrafo para testar fork v4.
-      </p>
-      <div>
-          Teste a funcionar o fork.
-      </div>
-      <p>
-         Paragrafo para testar fork v4.
-      </p>
-      <div>
-          Teste a funcionar o fork.
-      </div>
-      <p>
-         Paragrafo para testar fork v4.
-      </p>
-      <div>
-          Teste a funcionar o fork.
-      </div>
-      <p>
-         Paragrafo para testar fork v4.
-      </p>
-      <div>
-          Teste a funcionar o fork.
-      </div>
       <p>
          Paragrafo para testar fork v4.
       </p>
